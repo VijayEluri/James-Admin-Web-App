578972d6c28f1383f8dd3b29155157dc78a2cf2c
emacadie <eric@MacAdie.net>
2011-10-06 22:18:11 -0500

    InboxCountHolder now final.

--- src/main/java/org/apache/james/admin/webapp/beans/InboxCountHolder.java
+++ src/main/java/org/apache/james/admin/webapp/beans/InboxCountHolder.java
@@ -1,31 +1,18 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.james.admin.webapp.beans;
 
 /**
  *
  * @author ericm
  */
-public class InboxCountHolder {
+public final class InboxCountHolder {
 
-    public InboxCountHolder() {
-
+    public InboxCountHolder( String userNameA, Long messageCountA ) {
+        userName = userNameA;
+        messageCount = new Long( messageCountA.longValue() );
     }
 
     private String userName;
-    private Long messageCount;
+    private Long   messageCount;
 
     /**
      * @return the userName
@@ -37,22 +24,26 @@ public class InboxCountHolder {
     /**
      * @param userName the userName to set
      */
+    /*
     public void setUserName( String userName ) {
         this.userName = userName;
     }
-
+     * */
     /**
      * @return the messageCount
      */
     public Long getMessageCount() {
-        return messageCount;
+        // return messageCount;
+        return new Long( messageCount.longValue() );
     }
 
     /**
      * @param messageCount the messageCount to set
      */
+    /*
     public void setMessageCount( Long messageCount ) {
         this.messageCount = messageCount;
     }
+    */
+} // end class org.apache.james.admin.webapp.beans.InboxCountHolder
 
-} // end class info.shelfunit.james.web.InboxCountHolder

14401e3095278f250f151347e3705d03614c9ff7
emacadie <eric@MacAdie.net>
2011-10-11 23:04:55 -0500

    Adding the Hibernate xml files and updated NewHibernateUtil

--- src/main/java/org/apache/james/admin/webapp/hibernate/NewHibernateUtil.java
+++ src/main/java/org/apache/james/admin/webapp/hibernate/NewHibernateUtil.java
@@ -1,23 +1,10 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
-
 package org.apache.james.admin.webapp.hibernate;
 
-
-import org.hibernate.cfg.Configuration;
-// ORIG:
-// import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
 
 /**
@@ -26,22 +13,21 @@ import org.hibernate.SessionFactory;
  * @author ericm
  */
 public class NewHibernateUtil {
-    private static final SessionFactory sessionFactory;
 
+    private static final SessionFactory sessionFactory;
+    
     static {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-            // orig:
-            // sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-        } catch ( Throwable ex ) {
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
             // Log the exception. 
-            System.err.println( "Initial SessionFactory creation failed." + ex );
-            throw new ExceptionInInitializerError( ex );
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
         }
     }
-
+    
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
